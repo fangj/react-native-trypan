f8be07fc667f245305811c525c79659f3efd63b8
Fang Jian <fangjianmail@126.com>
2016-10-25 15:46:57 +0800

    test in real matchine ok

--- app/components/App3.js
+++ app/components/App3.js
@@ -1,29 +1,27 @@
-/**
- * React Native for Web Starter App
- * https://github.com/grabcode/react-native-web-starter
- * Follow me https://twitter.com/grabthecode
- */
-
 import React, { Component } from 'react';
+
 import {
   StyleSheet,
   Text,
   View,
   Image,
   PanResponder,
+  Dimensions,
+  ToastAndroid
 } from 'react-native';
+const { width, height } = Dimensions.get('window')
 
-import { Touchable } from './Touchable';
 
 export class App extends Component {
   componentWillMount() {
+    const me=this;
     this._panResponder = PanResponder.create({
       // Ask to be the responder:
       onStartShouldSetPanResponder: (evt, gestureState) => false,
       onStartShouldSetPanResponderCapture: (evt, gestureState) => false,
       onMoveShouldSetPanResponder: (evt, gestureState) => {
         const dx=Math.abs(gestureState.dx);
-        console.log('onMoveShouldSetPanResponder',dx);
+        // console.log('onMoveShouldSetPanResponder',dx);
         return dx>20;//发生大于20的水平偏移时才作为响应者
       },
       onMoveShouldSetPanResponderCapture: (evt, gestureState) => false,
@@ -41,6 +39,8 @@ export class App extends Component {
         // gestureState.d{x,y}
         const dx=gestureState.dx;
         console.log('onPanResponderMove',dx);
+        me.accept("contX",dx);
+        // ToastAndroid.show('dx '+dx, ToastAndroid.SHORT);
       },
       onPanResponderTerminationRequest: (evt, gestureState) => true,
       onPanResponderRelease: (evt, gestureState) => {
@@ -59,15 +59,23 @@ export class App extends Component {
     });
   }
 
+  constructor(props){
+    super(props);
+    this.state={}
+    this.accept=this.accept.bind(this);
+  }
+
   render() {
+    console.log(styles)
+    var contX=this.state.contX||0;
     return (
-      <View style={styles.container}>
-        <View style={styles.leftlist}>
+      <View style={[containerStyle,{left:contX}]}>
+        <View style={styles.leftlist} {...this._panResponder.panHandlers}>
           <View style={styles.card}  >
             <Text style={styles.text}>1</Text>
           </View>
         </View>
-        <View style={styles.rightlist}>
+        <View style={styles.rightlist} >
           <View style={styles.card}>
             <Text style={styles.text}>2</Text>
           </View>
@@ -75,30 +83,48 @@ export class App extends Component {
       </View>
     );
   }
+
+  accept(msg,data){
+    var state=this.state||{}; //获取当前的state值
+    const fns={
+      "contX":setContainerX, //响应msg的函数列表
+    }
+    if(fns[msg]){ //如果有响应函数，用响应函数处理state后刷新组件
+      state=fns[msg](state,data,msg);
+      this.setState(state);
+    }
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
+function setContainerX(state,data,msg) {
+  state.contX=data;
+  return state;
+}
+
+var containerStyle={
     position:"relative",
     backgroundColor: '#F5FCFF',
-    height:"100%"
-  },
+    height:height,
+    width:width*2
+  };
+const styles = StyleSheet.create({
+
   leftlist:{
     position:"absolute",
     backgroundColor: 'lightgreen',
-    width:"100%",
-    height:"100%"
+    width:width,
+    height:height
   },
   rightlist:{
     position:"absolute",
     backgroundColor: 'lightblue',
-    width:"100%",
-    height:"100%",
-    left:"100%",
+    width:width,
+    height:height,
+    left:width,
   },
   card:{
-    width:"100%",
-    height:"100%",
+    width:width,
+    height:height,
     borderWidth: 2,
     borderColor: 'gray',
     borderStyle:'dashed',
@@ -107,6 +133,15 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   } ,
   text:{
-    fontSize:"72px"
+    fontSize:72
   }
 });
+
+import {AppRegistry } from 'react-native';
+AppRegistry.registerComponent('AwesomeProject', () => App);
+
+
+
+// import {AppRegistry } from 'react-native';
+// AppRegistry.registerComponent('AwesomeProject', () => App);
+
